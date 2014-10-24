a548b6a426ef141d493b6a10d6f50668df9838fc
Paul Frazee <pfrazee@gmail.com>
2014-10-24 18:12:29 -0500

    added new positioning functions (fromCenterX,Y)

--- src/positioning.js
+++ src/positioning.js
@@ -11,3 +11,17 @@ exports.fromBottom = function(y) {
     return this.parent.getHeight() - y
   }
 }
+
+exports.fromCenterX = function(x) {
+  x = x || 0
+  return function() {
+    return (this.parent.getWidth()/2) - this.getWidth() + x
+  }
+}
+
+exports.fromCenterY = function(y) {
+  y = y || 0
+  return function() {
+    return (this.parent.getHeight()/2) - this.getHeight() + y
+  }
+}
